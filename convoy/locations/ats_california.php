9b86aa137adc0b99eb49ea5ee227ddd55df263ca
StuartD <stuartajd@users.noreply.github.com>
2016-02-07 18:39:17 +0000

    Create ats_california.php

---
+++ convoy/locations/ats_california.php
@@ -0,0 +1,60 @@
+[{
+    "city":"Bakersfield"
+},
+{
+    "city":"Barstow"
+},
+{
+    "city":"Carlsbad"
+},
+{
+    "city":"El Centro"
+},
+{
+    "city":"Eureka"
+},
+{
+    "city":"Fresno"
+},
+{
+    "city":"Hornbrook"
+},
+{
+    "city":"Huron"
+},
+{
+    "city":"Los Angeles"
+},
+{
+    "city":"Oakdale"
+},
+{
+    "city":"Oakland"
+},
+{
+    "city":"Oxnard"
+},
+{
+    "city":"Redding"
+},
+{
+    "city":"Sacramento"
+},
+{
+    "city":"San Diego"
+},
+{
+    "city":"San Francisco"
+},
+{
+    "city":"San Rafael"
+},
+{
+    "city":"Santa Cruz"
+},
+{
+    "city":"Stockton"
+},
+{
+    "city":"Truckee"
+}]
