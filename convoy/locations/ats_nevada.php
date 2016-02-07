46227b209e529ead1f767afc7be51e2515208a42
StuartD <stuartajd@users.noreply.github.com>
2016-02-07 18:38:44 +0000

    Create ats_nevada.php

---
+++ convoy/locations/ats_nevada.php
@@ -0,0 +1,30 @@
+[{
+    "city":"Carson City"
+},
+{
+    "city":"Elko"
+},
+{
+    "city":"Ely"
+},
+{
+    "city":"Jackpot"
+},
+{
+    "city":"Las Vegas"
+},
+{
+    "city":"Pioche"
+},
+{
+    "city":"Primm"
+},
+{
+    "city":"Reno"
+},
+{
+    "city":"Tonopah"
+},
+{
+    "city":"Winnemucca"
+}]
